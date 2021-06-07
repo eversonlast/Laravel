8f793da5d278857bb784dca47084eed41d2ed79f
EVERSON LUIZ GALVAO ABREU <everson.abreu@administrativo.unitau-dmn.br>
2021-06-07 10:51:09 -0300

    ep11

---
+++ Projeto/ep11/resources/views/home.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laravel + Bootstrap</title>
+    <link rel="stylesheet" href="{{asset('site/style.css')}}">
+</head>
+<body>
+    <h1>Testando Bootstrap</h1>
+
+    <button class="btn btn-lg btn-outline-orange">OK!</button>
+
+    <script src="{{asset('site/jquery.min.js')}}"></script>
+    <script src="{{asset('site/bootstrap.bundle.min.js')}}"></script>
+</body>
+</html>
